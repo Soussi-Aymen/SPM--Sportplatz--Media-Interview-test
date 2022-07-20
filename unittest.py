45e637978b89be9ae34b9215172e247487011a7e
Soussi-Aymen <aymensoussi.02@gmail.com>
2022-07-20 21:49:09 +0100

    change the structure of unit test file

--- unittest.py
+++ unittest.py
@@ -4,9 +4,14 @@ import main-program
 class TestProgram(unittest.TestCase):
 inputs = ['3','20.05.2022','15:00','21.05.2022','12:00','22.05.2022','07:00']
   
+  #unit test function for read_input function
+  def test_read_input(self):
+  
+  #unit test function for read_input function
+  def test_write_output(self):
+ 
+  #unit test function for read_input function  
   def test_main(self):
-    self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-
+    
 if __name__ == '__main__':
     unittest.main()
