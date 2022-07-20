0b41c0fdbac5cffb4a3985245575e17e4abaa387
Soussi-Aymen <aymensoussi.02@gmail.com>
2022-07-20 21:51:24 +0100

    python code structred in unittest file

--- unittest.py
+++ unittest.py
@@ -2,7 +2,7 @@ import unittest
 import main-program
 
 class TestProgram(unittest.TestCase):
-inputs = ['3','20.05.2022','15:00','21.05.2022','12:00','22.05.2022','07:00']
+  inputs = ['3','20.05.2022','15:00','21.05.2022','12:00','22.05.2022','07:00']
   
   #unit test function for read_input function
   def test_read_input(self):
@@ -14,4 +14,4 @@ inputs = ['3','20.05.2022','15:00','21.05.2022','12:00','22.05.2022','07:00']
   def test_main(self):
     
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
